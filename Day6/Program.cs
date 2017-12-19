65c95429ff4f69b6910aa3f4f0e2b85a6e3f8c3b
Trent Jones <trentwilliamjones@gmail.com>
2017-12-19 22:07:05 +0800

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			const string PUZZLEINPUT = "0\t5\t10\t0\t11\t14\t13\t4\t11\t8\t8\t7\t1\t4\t12\t11";
+
+			Redistributer redistributer = new Redistributer();
+			int cycles = redistributer.RedistributeUntilDuplicateState(PUZZLEINPUT.Split('\t'));
+			Console.WriteLine($"Part 1: {cycles}");
+
+			cycles = redistributer.FindCycles(PUZZLEINPUT.Split('\t'));
+			Console.WriteLine($"Part 2: {cycles}");
+
+			Console.ReadKey();
+		}
+	}
+
+	internal class Redistributer
+	{
+		public int RedistributeUntilDuplicateState(string[] memoryBanks)
+		{
+			List<string> memoryBankHistory = new List<string>();
+			List<int> memoryBanksConverted = memoryBanks.Select(int.Parse).ToList();
+			int cycles = 0;
+
+			while (!memoryBankHistory.Contains(string.Join(" ", memoryBanksConverted.ToArray())))
+			{
+				memoryBankHistory.Add(string.Join(" ", memoryBanksConverted.ToArray()));
+
+				int largestBank = memoryBanksConverted.Max();
+				int largestBankIndex = memoryBanksConverted.IndexOf(largestBank);
+
+				memoryBanksConverted[largestBankIndex] = 0;
+
+				while (largestBank > 0)
+				{
+					largestBankIndex = largestBankIndex == memoryBanksConverted.Count - 1 ? 0 : largestBankIndex + 1;
+					memoryBanksConverted[largestBankIndex]++;
+					largestBank--;
+				}
+
+				cycles++;
+			}
+
+			return cycles;
+		}
+
+		public int FindCycles(string[] memoryBanks)
+		{
+			List<string> memoryBankHistory = new List<string>();
+			List<int> memoryBanksConverted = memoryBanks.Select(int.Parse).ToList();
+			int cycles = 0;
+
+			while (!memoryBankHistory.Contains(string.Join(" ", memoryBanksConverted.ToArray())))
+			{
+				memoryBankHistory.Add(string.Join(" ", memoryBanksConverted.ToArray()));
+
+				int largestBank = memoryBanksConverted.Max();
+				int largestBankIndex = memoryBanksConverted.IndexOf(largestBank);
+
+				memoryBanksConverted[largestBankIndex] = 0;
+
+				while (largestBank > 0)
+				{
+					largestBankIndex = largestBankIndex == memoryBanksConverted.Count - 1 ? 0 : largestBankIndex + 1;
+					memoryBanksConverted[largestBankIndex]++;
+					largestBank--;
+				}
+
+				cycles++;
+			}
+
+			return cycles - memoryBankHistory.IndexOf(string.Join(" ", memoryBanksConverted.ToArray()));
+		}
+	}
+}
---
+++ Day6/UnitTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Day6
+{
+	[TestFixture]
+	internal class UnitTests
+	{
+		[TestCase(new[] {"1", "1"}, 2, TestName = "Basic")]
+		[TestCase(new[] {"0", "2", "7", "0"}, 5, TestName = "Part1_Example")]
+		public void RedistributeUntilDuplicateState_ReturnsExpectedResult(string[] memoryBanks, int expectedResult)
+		{
+			// Arrange
+			Redistributer sut = new Redistributer();
+
+			// Act
+			int cycles = sut.RedistributeUntilDuplicateState(memoryBanks);
+
+			// Assert
+			Assert.That(cycles, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase(new[] { "0", "2", "7", "0" }, 4, TestName = "Part2_Example")]
+		public void FindCycles_ReturnsExpectedResult(string[] memoryBanks, int expectedResult)
+		{
+			// Arrange
+			Redistributer sut = new Redistributer();
+
+			// Act
+			int cycles = sut.FindCycles(memoryBanks);
+
+			// Assert
+			Assert.That(cycles, Is.EqualTo(expectedResult));
+		}
+	}
+}
