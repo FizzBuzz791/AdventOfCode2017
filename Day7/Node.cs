73a488bb41a1baa334c67a5c27203edf3ced54b2
Trent Jones <trentwilliamjones@gmail.com>
2017-12-20 16:04:09 +0800

    Day 7

---
+++ Day7/Node.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Day7
+{
+	[Serializable]
+	// ReSharper disable once UseNameofExpression
+	[DebuggerDisplay("Name: {Name} | Weight: {Weight} | Child Nodes: {ChildNodes.Count} | Balanced: {StackBalanced}")]
+    public class Node
+    {
+        public string Name { get; }
+        public int Weight { get; }
+        public List<Node> ChildNodes { get; }
+
+		public bool HasChildren => ChildNodes.Count > 0;
+
+	    public bool StackBalanced
+	    {
+		    get
+		    {
+			    bool balanced = true;
+
+			    if (HasChildren)
+			    {
+						List<int> weights = new List<int>();
+						foreach (Node childNode in ChildNodes)
+						{
+							weights.Add(childNode.Weight + childNode.ChildWeight);
+					    }
+
+					    balanced = !weights.Distinct().Skip(1).Any();
+			    }
+
+			    return balanced;
+		    }
+	    }
+
+	    public int ChildWeight
+	    {
+		    get { return ChildNodes.Any() ? ChildNodes.Sum(c => c.Weight + c.ChildWeight) : 0; }
+	    }
+
+		public Node(string name, int weight, List<Node> childNodes)
+        {
+            Name = name;
+            Weight = weight;
+            ChildNodes = childNodes;
+        }
+
+        public static Node Parse(string nodeDescription)
+        {
+            string[] tokens = nodeDescription.Split(' ');
+
+            string name = tokens[0].Trim(',');
+            int weight = 0;
+
+            if (tokens.Length > 1)
+            {
+                weight = int.Parse(tokens[1].Trim('(').Trim(')'));
+            }
+
+            List<Node> childNodes = new List<Node>();
+            if (tokens.Length > 2)
+            {
+                int childTokenCount = tokens.Length - 3;
+                string[] childNodeTokens = new string[childTokenCount];
+                Array.Copy(tokens, 3, childNodeTokens, 0, childNodeTokens.Length);
+                childNodes.AddRange(childNodeTokens.Select(Parse));
+            }
+
+            Node node = new Node(name, weight, childNodes);
+
+            return node;
+        }
+
+        public static Node FindBaseNode(List<Node> nodes)
+        {
+			List<Node> seenNodes = new List<Node>();
+	        foreach (Node node in nodes.Where(n => n.HasChildren))
+	        {
+		        seenNodes.AddRange(node.ChildNodes);
+	        }
+
+	        Node finalNode = null;
+	        foreach (Node node in nodes)
+	        {
+		        if (seenNodes.All(n => n.Name != node.Name))
+		        {
+			        finalNode = node;
+		        }
+	        }
+
+	        return finalNode;
+        }
+
+	    public static Node BuildTower(Node baseNode, List<Node> nodes)
+	    {
+			List<Node> fullChildNodes = new List<Node>();
+		    foreach (Node childNode in baseNode.ChildNodes)
+		    {
+			    Node fullChildNode = nodes.SingleOrDefault(n => n.Name == childNode.Name);
+			    if (fullChildNode != null)
+			    {
+				    if (fullChildNode.HasChildren)
+				    {
+					    fullChildNode = BuildTower(fullChildNode, nodes);
+				    }
+
+					fullChildNodes.Add(DeepClone(fullChildNode));
+				}
+		    }
+			baseNode.ChildNodes.Clear();
+		    baseNode.ChildNodes.AddRange(fullChildNodes);
+
+		    return baseNode;
+	    }
+
+	    public static Node DeepClone(Node nodeToClone)
+	    {
+			using (MemoryStream stream = new MemoryStream())
+			{
+				BinaryFormatter formatter = new BinaryFormatter();
+				formatter.Serialize(stream, nodeToClone);
+				stream.Position = 0;
+				return (Node)formatter.Deserialize(stream);
+			}
+		}
+
+	    public static int FindTargetWeight(Node tower)
+	    {
+		    int unbalancedWeight = -1;
+
+			if (!tower.StackBalanced)
+			{
+				if (tower.ChildNodes.All(c => c.StackBalanced))
+				{
+					List<int> weights = new List<int>();
+					foreach (Node childNode in tower.ChildNodes)
+					{
+						weights.Add(childNode.Weight + childNode.ChildWeight);
+					}
+
+					List<IGrouping<int, int>> distinctWeights = weights.GroupBy(w => w).ToList();
+					if (distinctWeights.Count > 1)
+					{
+						int targetWeight = 0;
+						int unbalanced = 0;
+						foreach (IGrouping<int, int> distinctWeight in distinctWeights)
+						{
+							if (distinctWeight.Count() > 1)
+								targetWeight = distinctWeight.First();
+							else
+								unbalanced = distinctWeight.First();
+						}
+
+						Node targetNode = tower.ChildNodes.Single(n => n.Weight + n.ChildWeight == unbalanced);
+						int weightDifference = targetWeight - targetNode.ChildWeight - targetNode.Weight;
+						unbalancedWeight = targetNode.Weight + weightDifference;
+					}
+				}
+				else
+				{
+					unbalancedWeight = FindTargetWeight(tower.ChildNodes.Single(c => !c.StackBalanced));
+				}
+			}
+
+		    return unbalancedWeight;
+	    }
+    }
+}
---
+++ Day7/UnitTests.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using NUnit.Framework;
+
+namespace Day7
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase("bxlur (38)", "bxlur", 38, 0, TestName = "NoChildren")]
+        [TestCase("ehsqyyb (174) -> xtcdt", "ehsqyyb", 174, 1, TestName = "OneChild")]
+        [TestCase("ehsqyyb (174) -> xtcdt, tujcuy, wiqohmb, cxdwmu", "ehsqyyb", 174, 4, TestName = "MultipleChildren")]
+        public void Parse_CorrectlyInterprets_Description(string nodeDescription, string name, int weight, int childNodeCount)
+        {
+            // Arrange
+            // Nothing to do.
+
+            // Act
+            Node result = Node.Parse(nodeDescription);
+
+            // Assert
+            Assert.That(result.Name, Is.EqualTo(name));
+            Assert.That(result.Weight, Is.EqualTo(weight));
+            Assert.That(result.ChildNodes.Count, Is.EqualTo(childNodeCount));
+        }
+
+        [TestCase]
+        public void FindBaseNode_ReturnsExpectedResult()
+        {
+            // Arrange
+            List<Node> flatNodeList = new List<Node>
+            {
+                new Node("2", 2, new List<Node> {new Node("3", 0, new List<Node>())}),
+				new Node("3", 3, new List<Node>())
+            };
+
+            // Act
+            Node tower = Node.FindBaseNode(flatNodeList);
+
+            // Assert
+            Assert.That(tower.Name, Is.EqualTo("2"));
+        }
+
+		[TestCase]
+	    public void DeepClone_FunctionsCorrectly()
+	    {
+			// Arrange
+			Node node = new Node("1", 1, new List<Node>());
+
+			// Act
+		    Node clone = Node.DeepClone(node);
+
+		    // Assert
+			Assert.That(node, Is.Not.EqualTo(clone));
+	    }
+
+		[TestCase]
+	    public void BuildTower_ConstructsFullTower_Correctly()
+	    {
+			// Arrange
+			List<Node> flatNodeList = new List<Node>
+			{
+				new Node("pbga", 66, new List<Node>()),
+				new Node("xhth", 57, new List<Node>()),
+				new Node("ebii", 61, new List<Node>()),
+				new Node("havc", 66, new List<Node>()),
+				new Node("ktlj", 57, new List<Node>()),
+				new Node("fwft", 72, new List<Node>
+				{
+					new Node("ktlj", 0, new List<Node>()),
+					new Node("cntj", 0, new List<Node>()),
+					new Node("xhth", 0, new List<Node>())
+				}),
+				new Node("qoyq", 66, new List<Node>()),
+				new Node("padx", 45, new List<Node>
+				{
+					new Node("pbga", 0, new List<Node>()),
+					new Node("havc", 0, new List<Node>()),
+					new Node("qoyq", 0, new List<Node>())
+				}),
+				new Node("tknk", 41, new List<Node>
+				{
+					new Node("ugml", 0, new List<Node>()),
+					new Node("padx", 0, new List<Node>()),
+					new Node("fwft", 0, new List<Node>())
+				}),
+				new Node("jptl", 61, new List<Node>()),
+				new Node("ugml", 68, new List<Node>
+				{
+					new Node("gyxo", 0, new List<Node>()),
+					new Node("ebii", 0, new List<Node>()),
+					new Node("jptl", 0, new List<Node>())
+				}),
+				new Node("gyxo", 61, new List<Node>()),
+				new Node("cntj", 57, new List<Node>())
+			};
+
+			// Act
+		    Node baseNode = Node.FindBaseNode(flatNodeList);
+		    baseNode = Node.BuildTower(baseNode, flatNodeList);
+
+		    // Assert
+			Assert.That(baseNode.Name, Is.EqualTo("tknk"));
+			Assert.That(baseNode.ChildNodes.Count, Is.EqualTo(3));
+		    foreach (Node childNode in baseNode.ChildNodes)
+		    {
+				Assert.That(childNode.ChildNodes.Count, Is.EqualTo(3));
+		    }
+	    }
+
+		[TestCaseSource(nameof(StackBalancingData))]
+		public void StackBalanced_DeterminesBalanceCorrectly(Node sourceNode, bool expectedResult)
+	    {
+			// Arrange
+
+			// Act
+
+			// Assert
+			Assert.That(sourceNode.StackBalanced, Is.EqualTo(expectedResult));
+	    }
+
+		[TestCaseSource(nameof(TargetWeightData))]
+	    public void FindUnbalancedWeight_CorrectlyIdentifiesUnbalancedWeight(Node tower, int expectedResult)
+	    {
+			Assert.That(Node.FindTargetWeight(tower), Is.EqualTo(expectedResult));
+	    }
+
+	    private static IEnumerable<TestCaseData> StackBalancingData
+	    {
+		    [UsedImplicitly]
+		    get
+		    {
+			    yield return new TestCaseData(new Node("1", 1, new List<Node>()), true) {TestName = "Empty_Stack"};
+			    yield return new TestCaseData(new Node("1", 1, new List<Node>
+			    {
+				    new Node("2", 2, new List<Node>()),
+				    new Node("3", 2, new List<Node>())
+			    }), true) {TestName = "Simple_Stack_Balanced"};
+			    yield return new TestCaseData(new Node("1", 1, new List<Node>
+			    {
+				    new Node("2", 2, new List<Node>()),
+				    new Node("3", 3, new List<Node>())
+			    }), false) {TestName = "Simple_Stack_Unbalanced"};
+			    yield return new TestCaseData(new Node("tknk", 41, new List<Node>
+			    {
+				    new Node("ugml", 68, new List<Node>
+				    {
+					    new Node("gyxo", 61, new List<Node>()),
+					    new Node("ebii", 61, new List<Node>()),
+					    new Node("jptl", 61, new List<Node>())
+				    }),
+					new Node("padx", 45, new List<Node>
+					{
+						new Node("pbga", 66, new List<Node>()),
+						new Node("havc", 66, new List<Node>()),
+						new Node("qoyq", 66, new List<Node>())
+					}),
+					new Node("fwft", 72, new List<Node>
+					{
+						new Node("ktlj", 57, new List<Node>()),
+						new Node("cntj", 57, new List<Node>()),
+						new Node("xhth", 57, new List<Node>())
+					})
+			    }), false) {TestName = "Sample_Unbalanced"};
+		    }
+	    }
+
+	    private static IEnumerable<TestCaseData> TargetWeightData
+	    {
+		    [UsedImplicitly]
+		    get
+		    {
+			    yield return new TestCaseData(new Node("1", 1, new List<Node>()), -1) { TestName = "Empty_Stack" };
+			    yield return new TestCaseData(new Node("tknk", 41, new List<Node>
+				    {
+					    new Node("ugml", 68, new List<Node>
+					    {
+						    new Node("gyxo", 61, new List<Node>()),
+						    new Node("ebii", 61, new List<Node>()),
+						    new Node("jptl", 61, new List<Node>())
+					    }),
+					    new Node("padx", 45, new List<Node>
+					    {
+						    new Node("pbga", 66, new List<Node>()),
+						    new Node("havc", 66, new List<Node>()),
+						    new Node("qoyq", 66, new List<Node>())
+					    }),
+					    new Node("fwft", 72, new List<Node>
+					    {
+						    new Node("ktlj", 57, new List<Node>()),
+						    new Node("cntj", 57, new List<Node>()),
+						    new Node("xhth", 57, new List<Node>())
+					    })
+				    }), 60)
+				    { TestName = "Sample_Unbalanced" };
+		    }
+	    }
+	}
+}
