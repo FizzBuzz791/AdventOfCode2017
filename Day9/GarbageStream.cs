bb118fb684262fc37d95622966ce3ae7b96ca3d3
Trent Jones <trentwilliamjones@gmail.com>
2017-12-29 08:07:07 +0800

    Day 9

---
+++ Day9/GarbageStream.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+
+namespace Day9
+{
+	public class GarbageStreamReader : StringReader
+	{
+		public int Level { get; private set; }
+		public int GroupScore { get; private set; }
+		public int GarbageCount { get; private set; }
+
+		private int _previousCharacter;
+		private bool _inGarbage;
+
+		public GarbageStreamReader(string s) : base(s)
+		{
+			Level = 0;
+			GroupScore = 0;
+			GarbageCount = 0;
+
+			_inGarbage = false;
+		}
+
+		public override int Read()
+		{
+			int result = base.Read();
+
+			if (_previousCharacter != 33) // ! or !!
+			{
+				switch (result)
+				{
+					case 33: // !
+						// Do Nothing
+						break;
+					case 60: // <
+						if (_inGarbage)
+							GarbageCount++;
+						else
+							_inGarbage = true;
+						break;
+					case 62: // >
+						if (_inGarbage)
+							_inGarbage = false;
+						break;
+					case 123: // {
+						if (_inGarbage)
+							GarbageCount++;
+						else
+							Level++;
+						break;
+					case 125: // }
+						if (Level > 0 && !_inGarbage)
+						{
+							GroupScore += Level;
+							Level--;
+						}
+						else if (_inGarbage)
+							GarbageCount++;
+						break;
+					default:
+						if (_inGarbage)
+							GarbageCount++;
+						break;
+				}
+			}
+
+			if (_previousCharacter == 33 && result == 33 || _inGarbage && result != 33)
+				_previousCharacter = 0;
+			else
+				_previousCharacter = result;
+			
+			return result;
+		}
+	}
+}
---
+++ Day9/UnitTests.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+
+namespace Day9
+{
+	[TestFixture]
+	public class UnitTests
+	{
+		[TestCase("a", 0, TestName = "NoIncrement")]
+		[TestCase("{", 1, TestName = "Increment_OneLevel")]
+		[TestCase("{{", 2, TestName = "Increment_TwoLevels")]
+		[TestCase("!{", 0, TestName = "NoIncrement_Ignore")]
+		[TestCase("{!{", 1, TestName = "Increment_OneLevel_Ignore")]
+		public void Read_IncrementsLevel(string stream, int expectedResult)
+		{
+			// Arrange
+			GarbageStreamReader sut = new GarbageStreamReader(stream);
+
+			// Act
+			while (sut.Peek() != -1)
+			{
+				sut.Read();
+			}
+
+			// Assert
+			Assert.That(sut.Level, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase("a", 0, TestName = "NoDecrement")]
+		[TestCase("}", 0, TestName = "NoDecrement_Negative")]
+		[TestCase("{}", 0, TestName = "Decrement_OneLevel")]
+		[TestCase("{{}}", 0, TestName = "Decrement_TwoLevels")]
+		[TestCase("!{}", 0, TestName = "NoDecrement_IgnoreStart")]
+		[TestCase("{!}", 1, TestName = "NoDecrement_IgnoreEnd")]
+		public void Read_DecrementsLevel(string stream, int expectedResult)
+		{
+			// Arrange
+			GarbageStreamReader sut = new GarbageStreamReader(stream);
+
+			// Act
+			while (sut.Peek() != -1)
+			{
+				sut.Read();
+			}
+
+			// Assert
+			Assert.That(sut.Level, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase("", 0, TestName = "NoGroups")]
+		[TestCase("{}", 1, TestName = "OneGroup")]
+		[TestCase("{{}}", 3, TestName = "TwoGroups_Nested")]
+		[TestCase("{<a>}", 1, TestName = "OneGroup_Garbage")]
+		[TestCase("{<a>{<b>}<c>}", 3, TestName = "TwoGroups_Nested_Garbage")]
+		[TestCase("{{},{}}", 5, TestName = "ThreeGroups_InternalsNotNested")]
+		[TestCase("{{<},{>}}", 3, TestName = "ThreeGroups_InternalsNotNested")]
+		[TestCase("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9, TestName = "FiveGroups_InternalsNotNested_DoubleIgnores")]
+		[TestCase("{{{},{},{{}}}}", 16, TestName = "ComplexGroupings")]
+		[TestCase("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3, TestName = "GarbageIgnore")]
+		public void Read_CalculatesGroupScore(string stream, int expectedResult)
+		{
+			// Arrange
+			GarbageStreamReader sut = new GarbageStreamReader(stream);
+
+			// Act
+			while (sut.Peek() != -1)
+			{
+				sut.Read();
+			}
+
+			// Assert
+			Assert.That(sut.GroupScore, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase("<>", 0, TestName = "NoGarbage")]
+		[TestCase("<a>", 1, TestName = "OneGarbage")]
+		[TestCase("<<>", 1, TestName = "GarbageCharInGarbage")]
+		[TestCase("<!!>", 0, TestName = "DoubleIgnoreInGarbage")]
+		[TestCase("<{o\"i!a,<{i<a>", 10, TestName = "SampleGarbage")]
+		public void Read_CalculatesGarbageCount(string stream, int expectedResult)
+		{
+			// Arrange
+			GarbageStreamReader sut = new GarbageStreamReader(stream);
+
+			// Act
+			while (sut.Peek() != -1)
+			{
+				sut.Read();
+			}
+
+			// Assert
+			Assert.That(sut.GarbageCount, Is.EqualTo(expectedResult));
+		}
+	}
+}
