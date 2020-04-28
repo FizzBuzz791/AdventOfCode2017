1d0d83d6b7ce3aa1cd5d4d62e5ce161c69aa0abc
Trent Jones <trentwilliamjones@gmail.com>
2020-04-28 12:41:05 +0800

    Day 4 - Part 2

--- 2019/Day4/Solution/Program.cs
+++ 2019/Day4/Solution/Program.cs
@@ -14,15 +14,16 @@ namespace Day4
             var puzzle = new Puzzle(user, 2019, 4);
             var input = puzzle.GetInputAsync().Result;
 
-            Part1(input);
-        }
-
-        public static void Part1(string input)
-        {
             var range = input.Split("-");
             var lowerLimit = Int32.Parse(range[0]);
             var upperLimit = Int32.Parse(range[1]);
 
+            Part1(lowerLimit, upperLimit);
+            Part2(lowerLimit, upperLimit);
+        }
+
+        public static void Part1(int lowerLimit, int upperLimit)
+        {
             var validNumberCount = 0;
             for (int i = lowerLimit; i < upperLimit; i++)
             {
@@ -33,20 +34,40 @@ namespace Day4
             Console.WriteLine($"Part 1: {validNumberCount}");
         }
 
-        public static bool NumberIsValid(int number)
+        public static void Part2(int lowerLimit, int upperLimit)
         {
-            var hasDouble = false;
-            var onlyIncreases = true;
-            var numberParts = number.ToString().ToArray();
-            for (int i = 0; i < numberParts.Length - 1; i++)
+            var validNumberCount = 0;
+            for (int i = lowerLimit; i < upperLimit; i++)
             {
-                var firstPart = Convert.ToInt32(numberParts[i]);
-                var secondPart = Convert.ToInt32(numberParts[i + 1]);
-                if (firstPart == secondPart)
-                    hasDouble = true;
+                if (NumberIsValid(i, true))
+                    validNumberCount++;
+            }
 
-                if (secondPart < firstPart)
-                    onlyIncreases = false;
+            Console.WriteLine($"Part 2: {validNumberCount}");
+        }
+
+        public static bool NumberIsValid(int number, bool doPart2Checks = false)
+        {
+            var numberParts = number.ToString().ToArray();
+
+            var firstPart = Int32.Parse(numberParts[0].ToString());
+            var secondPart = Int32.Parse(numberParts[1].ToString());
+            var thirdPart = Int32.Parse(numberParts[2].ToString());
+            var fourthPart = Int32.Parse(numberParts[3].ToString());
+            var fifthPart = Int32.Parse(numberParts[4].ToString());
+            var sixthPart = Int32.Parse(numberParts[5].ToString());
+
+            bool hasDouble = firstPart == secondPart || secondPart == thirdPart || thirdPart == fourthPart || fourthPart == fifthPart || fifthPart == sixthPart;
+            bool onlyIncreases = firstPart <= secondPart && secondPart <= thirdPart && thirdPart <= fourthPart && fourthPart <= fifthPart && fifthPart <= sixthPart;
+
+            if (hasDouble && onlyIncreases && doPart2Checks)
+            {
+                var repeats = numberParts.GroupBy(p => numberParts.Count(n => n == p)).Where(g => g.Key >= 2);
+                if (repeats.Count() == 1 && repeats.First().Key != 2)
+                {
+                    // x111xx
+                    hasDouble = false;
+                }
             }
 
             return hasDouble && onlyIncreases;
--- 2019/Day4/Tests/Part2Tests.cs
+++ 2019/Day4/Tests/Part2Tests.cs
@@ -3,17 +3,17 @@ using Shouldly;
 
 namespace Day4Tests
 {
-    public class Part1Tests
+    public class Part2Tests
     {
-        [TestCase(111111, true)]
-        [TestCase(223450, false)]
-        [TestCase(123789, false)]
+        [TestCase(112233, true)]
+        [TestCase(123444, false)]
+        [TestCase(111122, true)]
         public void CanValidateNumbersCorrectly(int input, bool expectedResult)
         {
             // Arrange
 
             // Act
-            var numberIsValid = Day4.Program.NumberIsValid(input);
+            var numberIsValid = Day4.Program.NumberIsValid(input, true);
 
             // Assert
             numberIsValid.ShouldBe(expectedResult);
