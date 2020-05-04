cfabbe9717c21645b73b4d15cf5b8970a2874111
Trent Jones <trentwilliamjones@gmail.com>
2020-05-04 13:47:58 +0800

    Set up Day 8 base

---
+++ 2019/Day8/Solution/Program.cs
@@ -0,0 +1,14 @@
+﻿using NAoCHelper;
+
+namespace Day8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User(Helpers.GetCookie("f4e71f6f-ce28-49d3-8515-f4e51a2ddfd7"));
+            var puzzle = new Puzzle(user, 2019, 8);
+            var input = puzzle.GetInputAsync().Result;
+        }
+    }
+}
