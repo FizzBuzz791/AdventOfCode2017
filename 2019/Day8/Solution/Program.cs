5e9597dc44e31c50bf13912549b36200da21e67f
Trent Jones <trentwilliamjones@gmail.com>
2020-05-05 11:29:04 +0800

    Day 8 - Part 2

--- 2019/Day8/Solution/Program.cs
+++ 2019/Day8/Solution/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using NAoCHelper;
 
 namespace Day8
@@ -15,47 +16,66 @@ namespace Day8
 
             // Strip the newline
             input = input.Substring(0, input.Length - 1);
+            var layers = CalculateLayers(input, 25, 6);
 
-            var checksum = Part1(input, 25, 6);
+            var checksum = Part1(layers);
             Console.WriteLine($"Checksum is {checksum}.");
 
+            Console.WriteLine(Part2(layers, 25, 6));
         }
 
-        public static int Part1(string input, int width, int height)
+        public static Dictionary<int, List<int>> CalculateLayers(string input, int width, int height)
         {
             var imageData = new Queue<int>(input.ToCharArray().Select(c => Int32.Parse(c.ToString())).ToArray());
 
             var layers = new Dictionary<int, List<int>>();
             var layerCount = 0;
-            var indexOfLeastZeroes = -1;
-            var leastZeroes = Int32.MaxValue;
+
             while (imageData.Count > 0)
             {
                 var layer = new List<int>();
-                var zeroCount = 0;
                 for (int i = 0; i < width * height; i++)
                 {
-                    var digit = imageData.Dequeue();
-                    if (digit == 0)
-                        zeroCount++;
-
-                    layer.Add(digit);
+                    layer.Add(imageData.Dequeue());
                 }
 
                 layerCount++;
                 layers.Add(layerCount, layer);
-
-                if (zeroCount < leastZeroes)
-                {
-                    leastZeroes = zeroCount;
-                    indexOfLeastZeroes = layerCount;
-                }
             }
 
-            var onesCount = layers[indexOfLeastZeroes].Count(d => d == 1);
-            var twosCount = layers[indexOfLeastZeroes].Count(d => d == 2);
+            return layers;
+        }
+
+        public static int Part1(Dictionary<int, List<int>> layers)
+        {
+            var indexOfLeastZeroes = layers.Min(l => l.Value.Count(d => d == 0));
+            var layerWithLeastZeroes = layers[indexOfLeastZeroes];
+
+            var onesCount = layerWithLeastZeroes.Count(d => d == 1);
+            var twosCount = layerWithLeastZeroes.Count(d => d == 2);
 
             return onesCount * twosCount;
+        }
+
+        public static string Part2(Dictionary<int, List<int>> layers, int width, int height)
+        {
+            var output = new StringBuilder();
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    var pixels = new List<int>();
+                    foreach (var layer in layers)
+                    {
+                        pixels.Add(layer.Value[w + (h * width)]);
+                    }
+
+                    var pixel = pixels.FirstOrDefault(p => p == 0 || p == 1);
+                    output.Append(pixel == 0 ? " " : "#");
+                }
+                output.AppendLine();
+            }
+            return output.ToString();
         }
     }
 }
--- 2019/Day8/Tests/Part1Tests.cs
+++ 2019/Day8/Tests/Part1Tests.cs
@@ -4,15 +4,16 @@ using Shouldly;
 
 namespace Day8Tests
 {
-    public class Tests
+    public class Part1Tests
     {
         [TestCase("003006012012", 4)]
         public void FindsExpectedChecksum(string input, int expectedOutput)
         {
             // Arrange
+            var layers = Program.CalculateLayers(input, 3, 2);
 
             // Act
-            var checksum = Program.Part1(input, 3, 2);
+            var checksum = Program.Part1(layers);
 
             // Assert
             checksum.ShouldBe(expectedOutput);
