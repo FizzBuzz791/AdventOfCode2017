8a5f10ff9578ee8a7be644eb145b06b81ca8221d
Trent Jones <trentwilliamjones@gmail.com>
2020-05-06 14:19:56 +0800

    Set up Day 10 base

---
+++ 2019/Day10/Solution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NAoCHelper;
+
+namespace Day10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User(Helpers.GetCookie("c617493e-f753-4626-bb52-9661e64c9878"));
+            var puzzle = new Puzzle(user, 2019, 10);
+            var input = puzzle.GetInputAsync().Result;
+
+            Console.WriteLine(input);
+        }
+    }
+}
