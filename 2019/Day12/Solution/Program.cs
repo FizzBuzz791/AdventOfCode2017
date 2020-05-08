f69e8957e7b4765a8dec41bc81e46c2dc9d19817
Trent Jones <trentwilliamjones@gmail.com>
2020-05-08 12:31:58 +0800

    Set up Day 12 base

---
+++ 2019/Day12/Solution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NAoCHelper;
+
+namespace Day12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User(Helpers.GetCookie("c1614296-0e85-46b7-aed7-a1c3d3891e37"));
+            var puzzle = new Puzzle(user, 2019, 12);
+            var input = puzzle.GetInputAsync().Result;
+
+            Console.WriteLine(input);
+        }
+    }
+}
