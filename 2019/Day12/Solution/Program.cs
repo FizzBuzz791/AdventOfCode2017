a70aeea749fe372cb21de6d9c2ff42778e7ffec7
Trent Jones <trentwilliamjones@gmail.com>
2020-05-11 12:21:59 +0800

    Day 12 - Part 1

--- 2019/Day12/Solution/Program.cs
+++ 2019/Day12/Solution/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Combinatorics.Collections;
 using NAoCHelper;
 
 namespace Day12
@@ -11,7 +14,66 @@ namespace Day12
             var puzzle = new Puzzle(user, 2019, 12);
             var input = puzzle.GetInputAsync().Result;
 
-            Console.WriteLine(input);
+            // Strip the newline
+            input = input.Substring(0, input.Length - 1);
+
+            Part1(input, 1000);
+        }
+
+        public static List<Moon> Part1(string input, int steps)
+        {
+            var moons = input.Split("\n").Select(m => new Moon(m)).ToList();
+            var combinations = new Combinations<Moon>(moons, 2);
+
+            for (int i = 0; i < steps; i++)
+            {
+                foreach (var permutation in combinations)
+                    ApplyGravity(permutation.First(), permutation.Last());
+
+                foreach (var moon in moons)
+                    moon.ApplyVelocity();
+            }
+
+            var totalEnergy = moons.Sum(m => m.PotentialEnergy * m.KineticEnergy);
+            Console.WriteLine($"Sum of total energy after {steps} steps = {totalEnergy}");
+
+            return moons;
+        }
+
+        public static void ApplyGravity(Moon first, Moon second)
+        {
+            if (first.Position.X < second.Position.X)
+            {
+                first.Velocity.X++;
+                second.Velocity.X--;
+            }
+            else if (first.Position.X > second.Position.X)
+            {
+                first.Velocity.X--;
+                second.Velocity.X++;
+            }
+
+            if (first.Position.Y < second.Position.Y)
+            {
+                first.Velocity.Y++;
+                second.Velocity.Y--;
+            }
+            else if (first.Position.Y > second.Position.Y)
+            {
+                first.Velocity.Y--;
+                second.Velocity.Y++;
+            }
+
+            if (first.Position.Z < second.Position.Z)
+            {
+                first.Velocity.Z++;
+                second.Velocity.Z--;
+            }
+            else if (first.Position.Z > second.Position.Z)
+            {
+                first.Velocity.Z--;
+                second.Velocity.Z++;
+            }
         }
     }
 }
