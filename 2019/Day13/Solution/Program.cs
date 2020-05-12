254e622dc3a42fb5748895e184b9092ca4632b33
Trent Jones <trentwilliamjones@gmail.com>
2020-05-12 11:45:28 +0800

    Set up Day 13 base

---
+++ 2019/Day13/Solution/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using NAoCHelper;
+
+namespace Day13
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var user = new User(Helpers.GetCookie("af2ad03d-386b-4b2e-b3b5-ccb5f138b698"));
+            var puzzle = new Puzzle(user, 2019, 13);
+            var input = puzzle.GetInputAsync().Result;
+            var memory = input.Split(',').Select(BigInteger.Parse).ToArray();
+
+            Console.WriteLine(memory);
+        }
+    }
+}
