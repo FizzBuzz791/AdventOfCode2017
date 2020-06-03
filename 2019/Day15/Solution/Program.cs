d0b8c48b6cfa3ea3248cd0001da7e3e778a8e52e
Trent Jones <trentwilliamjones@gmail.com>
2020-06-03 12:12:38 +0800

    Set up Day 15 base

---
+++ 2019/Day15/Solution/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NAoCHelper;
+
+namespace Day15
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User(Helpers.GetCookie("f66a053b-e715-4168-b677-b51ee0115730"));
+            var puzzle = new Puzzle(user, 2019, 15);
+            var input = puzzle.GetInputAsync().Result;
+
+            // Strip the newline
+            input = input[0..^1];
+
+            Console.WriteLine(input);
+        }
+    }
+}
