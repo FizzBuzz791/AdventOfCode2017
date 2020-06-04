ab36d50bf2a2d25c086dcc8619d19b131d9296c0
Trent Jones <trentwilliamjones@gmail.com>
2020-06-04 15:57:34 +0800

    Day 15 - Part 1

--- 2019/Day15/Solution/Program.cs
+++ 2019/Day15/Solution/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
 using NAoCHelper;
 
 namespace Day15
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var user = new User(Helpers.GetCookie("f66a053b-e715-4168-b677-b51ee0115730"));
             var puzzle = new Puzzle(user, 2019, 15);
             var input = puzzle.GetInputAsync().Result;
+            var memory = input.Split(',').Select(BigInteger.Parse).ToArray();
 
-            // Strip the newline
-            input = input[0..^1];
+            Part1(memory);
+        }
 
-            Console.WriteLine(input);
+        public static void Part1(BigInteger[] memory)
+        {
+            var rdcs = new RepairDroidControlSystem(memory);
+            rdcs.Explore();
+
+            rdcs.DisplayGrid();
+
+            var shortestPath = rdcs.FindPath(rdcs.Origin, rdcs.Destination);
+            Console.WriteLine($"Shortest Path: {shortestPath}");
         }
     }
 }
