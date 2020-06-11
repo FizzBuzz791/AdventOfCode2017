e36da4b28543f0c11d44b1a42c30fe16ba7ebff0
Trent Jones <trentwilliamjones@gmail.com>
2020-06-11 19:10:27 +0800

    Set up Day 17 base

---
+++ 2019/Day17/Solution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NAoCHelper;
+
+namespace Day17
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var user = new User(Helpers.GetCookie("5755d29d-585d-46d4-ba5e-5eed30d1bcca"));
+            var puzzle = new Puzzle(user, 2019, 17);
+            var input = puzzle.GetInputAsync().Result.Trim('\n');
+
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ 2019/Day17/Tests/Part1Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Day17Tests
+{
+    public class Part1Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
