b62f6526f6ff20f9e44f5b0201042880851ef8dd
Trent Jones <trentwilliamjones@gmail.com>
2020-09-05 17:55:55 +0800

    Refactored Day 3 to new format

---
+++ 2019/Solutions/Day3/Dimensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.Day3
+{
+    public class Dimensions
+    {
+        public int MinX { get; }
+        public int MinY { get; }
+        public int MaxX { get; }
+        public int MaxY { get; }
+
+        public Dimensions(int minX, int minY, int maxX, int maxY)
+        {
+            MinX = minX;
+            MinY = minY;
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+    }
+}
